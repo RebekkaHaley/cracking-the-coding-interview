0c8d73d521943aa73ce671204bc5c4dbedb2119c
Rebekka Haley <rebekka.haley@gmail.com>
2022-09-28 15:27:16 +0200

    [tests] init pytest file for big o

---
+++ tests/test_big_o.py
@@ -0,0 +1,10 @@
+"""
+Tests for code related to the 'Big O' chapter in 'Cracking the Coding Interview'.
+"""
+
+from big_o import binary_search
+
+
+def test_binary_search_valid():
+    binary_search()
+    return None
