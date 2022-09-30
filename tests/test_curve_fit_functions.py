1a2c53ad7b0c33b2e3d63e4b45f532e197c067f6
Rebekka Haley <rebekka.haley@gmail.com>
2022-09-30 19:31:34 +0200

    [tests] populate curve fit placeholders

--- tests/test_curve_fit_functions.py
+++ tests/test_curve_fit_functions.py
@@ -1,3 +1,21 @@
 """
 Tests for code related to functions for use in scipy's curve fit.
 """
+
+from cmath import exp
+from cracking_the_coding_interview.curve_fit_functions import log_func, exp_func, lin_func
+
+
+def test_log_func():
+    log_func()
+    pass
+
+
+def test_exp_func():
+    exp_func()
+    pass
+
+
+def test_lin_func():
+    lin_func()
+    pass
