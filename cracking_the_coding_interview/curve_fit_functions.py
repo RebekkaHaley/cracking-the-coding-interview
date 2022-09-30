0aa944e16b0a714b83e7d489b8b8c5990f8d9162
Rebekka Haley <rebekka.haley@gmail.com>
2022-09-30 19:29:21 +0200

    [feat] populate with log exp and linear funcs

--- cracking_the_coding_interview/curve_fit_functions.py
+++ cracking_the_coding_interview/curve_fit_functions.py
@@ -1,3 +1,17 @@
 """
 Functions for use in scipy's curve fit.
 """
+
+import numpy as np
+
+
+def log_func(x, a, b, c):
+    return a*np.log(b + x)+c
+
+
+def exp_func(x, a, b, c):
+    return a*np.exp(-b*x)+c
+
+
+def lin_func(x, a, b):
+    return (a*x)+b
