2d43d027ef49d0486814bb71154964cbec0bf719
Rebekka Haley <rebekka.haley@gmail.com>
2022-10-06 17:47:32 +0200

    [feat/hashtable] added item setting functionality

--- cracking_the_coding_interview/data_sctructures.py
+++ cracking_the_coding_interview/data_sctructures.py
@@ -2,6 +2,8 @@
 Data structures coded from scratch.
 """
 
+BLANK = object()
+
 
 class HashTable:
     """Simple implementation of a hash table data structure using Python from scratch.
@@ -10,15 +12,29 @@ class HashTable:
         capacity: Size used to initiate array with empty values.
     """
     def __init__(self, capacity: int):
-        self.values = capacity * [None]
+        self.values = capacity * [BLANK]
 
     def __len__(self):
         """Returns capacity of array.
         """
         return len(self.values)
 
+    def __setitem__(self, key, value):
+        """todo
+
+        Args:
+            key: todo.
+            value: todo.
+        """
+        index = hash(key) % len(self)
+        self.values[index] = value
+
+
     def hash(self, key):
         """Gets index of array for a specific string key.
+
+        Args:
+            key: todo.
         """
         length = self.__len__
         return hash(key) % length
