7d8ad52bdb0f78a45adb7c9e916c3a0d1c1bbdb8
Rebekka Haley <rebekka.haley@gmail.com>
2022-10-06 17:52:51 +0200

    [test/refactor] rename test functions for clarity

--- tests/test_hash_table.py
+++ tests/test_hash_table.py
@@ -18,36 +18,36 @@ def hash_table():
     return sample_table
 
 
-def test_hashtable_should_create_valid():
+def test_should_create_hashtable():
     hash_table = HashTable(capacity=100)
     assert hash_table is not None
 
 
-def test_hashtable_should_not_contain_none_value_when_created():
+def test_should_not_contain_none_value_when_created():
     assert None not in HashTable(capacity=100).values
 
 
-def test_hashtable_should_report_capacity():
+def test_should_report_capacity():
     expected_value = 100
     hash_table = HashTable(capacity=expected_value)
     actual_value = len(hash_table)
     assert actual_value == expected_value
 
 
-def test_hashtable_should_populate_with_blank_obj():
+def test_should_populate_table_with_blank_obj():
     expected_values = [BLANK, BLANK, BLANK]
     hash_table = HashTable(capacity=3)
     actual_values = hash_table.values
     assert actual_values == expected_values
 
 
-def test_hashtable_should_insert_key_value_pairs(hash_table):
+def test_should_insert_key_value_pairs(hash_table):
     assert "world" in hash_table.values
     assert 37 in hash_table.values
     assert True in hash_table.values
 
 
-def test_hashtable_should_not_grow_when_adding_elements():
+def test_should_not_grow_when_adding_elements():
     expected_value = 100
     hash_table = HashTable(capacity=expected_value)
     hash_table["hello"] = "world"
@@ -56,19 +56,19 @@ def test_hashtable_should_not_grow_when_adding_elements():
 
 
 @pytest.mark.skip
-def test_hashtable_should_not_shrink_when_removing_elements():
+def test_should_not_shrink_when_removing_elements():
     hash_table = HashTable(capacity=100)
     pass
 
 
-def test_hashtable_should_insert_none_value():
+def test_should_insert_none_value():
     hash_table = HashTable(capacity=100)
     hash_table["key"] = None
     assert None in hash_table.values
 
 
 @pytest.mark.skip
-def test_hashtable_should_find_value_by_key(hash_table):
+def test_should_find_value_by_key(hash_table):
     assert hash_table["hola"] == "hello"
     assert hash_table[98.6] == 37
     assert hash_table[False] is True
