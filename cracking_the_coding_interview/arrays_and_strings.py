ab35b37f23673925697363cdffb1b2bde0a75a22
Rebekka Haley <rebekka.haley@gmail.com>
2022-10-06 16:59:55 +0200

    [refactor] rename func to is string chars unique

--- cracking_the_coding_interview/arrays_and_strings.py
+++ cracking_the_coding_interview/arrays_and_strings.py
@@ -5,7 +5,7 @@ Functions for section '1. Arrays and Strings' of 'Chapter IX: Interview Question
 import math
 
 
-def are_all_chars_unique(string: str) -> bool:
+def is_string_chars_unique(string: str) -> bool:
     """Calculates whether all chars in a given string appear only once.
 
     Args:
--- tests/test_arrays_and_strings.py
+++ tests/test_arrays_and_strings.py
@@ -3,11 +3,11 @@ Tests for section '1. Arrays and Strings' of 'Chapter IX: Interview Questions'.
 """
 
 from cracking_the_coding_interview.arrays_and_strings import (
-    are_all_chars_unique)
+    is_string_chars_unique)
 
 
 def test_are_all_chars_unique_valid_correct_outputs():
-    assert are_all_chars_unique(string='hello') is False
-    assert are_all_chars_unique(string='helo') is True
-    assert are_all_chars_unique(string='hel o') is True
-    assert are_all_chars_unique(string='hel  o') is False
+    assert is_string_chars_unique(string='hello') is False
+    assert is_string_chars_unique(string='helo') is True
+    assert is_string_chars_unique(string='hel o') is True
+    assert is_string_chars_unique(string='hel  o') is False
