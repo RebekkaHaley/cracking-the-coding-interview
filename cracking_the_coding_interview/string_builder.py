9f3e334ffbbd6c8019aa278d3ea90db4b8b9d78a
Rebekka Haley <rebekka.haley@gmail.com>
2022-10-17 13:47:16 +0200

    [feat/stringbuilder] Added classes and tests

--- cracking_the_coding_interview/string_builder.py
+++ cracking_the_coding_interview/string_builder.py
@@ -1,9 +1,45 @@
 """
-StringBuilder data structure coded from scratch. TODO.
+StringBuilder data structure coded from scratch.
 """
 
+from io import StringIO
 
-# class StringBuilder:
-#     """todo
-#     """
-#     pass
+
+class StringBuilder:
+    """StringBuilder data structure using list and join().
+    """
+    def __init__(self):
+        self.memory = []
+
+    def add(self, string: str):
+        """Appends string to StringBuilder.
+
+        Args:
+            string: Any string.
+        """
+        self.memory.append(string)
+
+    def __str__(self):
+        """Returns fully built string with all appended strings.
+        """
+        return "".join(self.memory)
+
+
+class StringBuilderIO:
+    """StringBuilder data structure using StringIO.
+    """
+    def __init__(self):
+        self.memory = StringIO()
+
+    def add(self, string: str):
+        """Appends string to StringBuilder.
+
+        Args:
+            string: Any string.
+        """
+        self.memory.write(string)
+
+    def __str__(self):
+        """Returns fully built string with all appended strings.
+        """
+        return self.memory.getvalue()
---
+++ tests/test_string_builder.py
@@ -0,0 +1,26 @@
+"""
+Tests StringBuilder data structure coded from scratch.
+"""
+
+import pytest
+
+from cracking_the_coding_interview.string_builder import StringBuilder
+
+
+def test_should_create_empty_string_builder():
+    expected_value = ""
+    string_builder = StringBuilder()
+    assert str(string_builder) == expected_value
+
+
+def test_should_not_create_stringbuilder_with_args():
+    with pytest.raises(TypeError):
+        StringBuilder("Hello ")
+
+
+def test_should_add_strings():
+    expected_value = "Hello World"
+    string_builder = StringBuilder()
+    string_builder.add("Hello ")
+    string_builder.add("World")
+    assert str(string_builder) == expected_value
