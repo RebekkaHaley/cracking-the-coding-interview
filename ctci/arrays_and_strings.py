e519aa02d8e3d0bd337f757c0373bff0791a4e27
Rebekka Haley <rebekka.haley@gmail.com>
2022-11-10 16:55:46 +0100

    [feat/q1.2] added check permutation logic

--- ctci/arrays_and_strings.py
+++ ctci/arrays_and_strings.py
@@ -2,8 +2,6 @@
 Functions for section '1. Arrays and Strings' of 'Chapter IX: Interview Questions'.
 """
 
-import math
-
 
 def is_unique(string: str) -> bool:
     """Checks whether all chars in a given string appear only once.
@@ -20,3 +18,18 @@ def is_unique(string: str) -> bool:
             return False
         memory = f'{memory}{char}'
     return True
+
+
+def check_permutation(string: str, target: str) -> bool:
+    """Checks whether one string is a permutation of the other.
+
+    Args:
+        string: Any string.
+        target: Potential permutation of 'string'.
+
+    Returns:
+        True if 'target' is a permutation of 'string'. Else False.
+    """
+    if sorted(string) == sorted(target):
+        return True
+    return False
--- tests/test_arrays_and_strings.py
+++ tests/test_arrays_and_strings.py
@@ -2,11 +2,16 @@
 Tests for section '1. Arrays and Strings' of 'Chapter IX: Interview Questions'.
 """
 
-from ctci.arrays_and_strings import is_unique
+from ctci.arrays_and_strings import is_unique, check_permutation
 
 
-def test_should_detect_string_with_unique_chars():
+def test_should_correctly_check_if_string_has_unique_chars():
     assert is_unique(string='hello') is False
     assert is_unique(string='helo') is True
     assert is_unique(string='hel o') is True
     assert is_unique(string='hel  o') is False
+
+
+def test_should_correctly_check_if_two_strings_are_permutations():
+    assert check_permutation(string='listen', target='silent') is True
+    assert check_permutation(string='foo', target='bar') is False
