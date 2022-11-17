3a54bd79f7996fb951643e24ec06c963ab566dfb
Rebekka Haley <rebekka.haley@gmail.com>
2022-11-17 18:59:16 +0100

    [tests] refactor old tests + add delete tests

--- tests/test_linked_lists.py
+++ tests/test_linked_lists.py
@@ -11,9 +11,9 @@ from ctci.linked_lists import Node, SinglyLinkedList
 @fixture
 def linked_list():
     sample_list = SinglyLinkedList()
-    sample_list.insert_tail(data=10)
-    sample_list.insert_tail(data=11)
-    sample_list.insert_tail(data=12)
+    sample_list.insert_head(data=12)
+    sample_list.insert_head(data=11)
+    sample_list.insert_head(data=10)
     return sample_list
 
 
@@ -57,15 +57,11 @@ def test_should_insert_data_node_to_empty_linked_list_at_head():
     assert l_list.head.next == None
 
 
-def test_should_insert_nodes_to_populated_linked_list_at_head():
-    l_list = SinglyLinkedList()
-    l_list.insert_head(data=12)
-    l_list.insert_head(data=11)
-    l_list.insert_head(data=10)
-    assert l_list.head.data == 10
-    assert l_list.head.next.data == 11
-    assert l_list.head.next.next.data == 12
-    assert l_list.head.next.next.next == None
+def test_should_insert_nodes_to_populated_linked_list_at_head(linked_list):
+    assert linked_list.head.data == 10
+    assert linked_list.head.next.data == 11
+    assert linked_list.head.next.next.data == 12
+    assert linked_list.head.next.next.next == None
 
 
 def test_should_insert_node_to_empty_linked_list_at_tail():
@@ -82,31 +78,71 @@ def test_should_insert_data_node_to_empty_linked_list_at_tail():
     assert l_list.head.next == None
 
 
-def test_should_insert_nodes_to_populated_linked_list_at_tail(linked_list):
-    assert linked_list.head.data == 10
-    assert linked_list.head.next.data == 11
-    assert linked_list.head.next.next.data == 12
-    assert linked_list.head.next.next.next == None
+def test_should_insert_nodes_to_populated_linked_list_at_tail():
+    l_list = SinglyLinkedList()
+    l_list.insert_tail(data=10)
+    l_list.insert_tail(data=11)
+    l_list.insert_tail(data=12)
+    assert l_list.head.data == 10
+    assert l_list.head.next.data == 11
+    assert l_list.head.next.next.data == 12
+    assert l_list.head.next.next.next == None
 
 
-def test_should_raise_error_when_inserting_after_on_an_empty_linked_list():
+def test_should_raise_error_when_inserting_after_to_an_empty_linked_list():
     l_list = SinglyLinkedList()
     with raises(ValueError):
         l_list.insert_after(data=1, target_data=1)
 
 
-def test_should_raise_error_when_inserting_after_using_missing_target(linked_list):
+def test_should_raise_error_when_inserting_after_using_a_missing_target(linked_list):
     with raises(ValueError):
         linked_list.insert_after(data=1, target_data=1)
 
 
-def test_should_insert_node_after_target_in_populated_linked_list(linked_list):
+def test_should_insert_node_after_target_in_a_populated_linked_list(linked_list):
     linked_list.insert_after(data=99, target_data=11)
     assert linked_list.head.data == 10
     assert linked_list.head.next.data == 11
     assert linked_list.head.next.next.data == 99
     assert linked_list.head.next.next.next.data == 12
     assert linked_list.head.next.next.next.next == None
+
+
+def test_should_raise_error_when_deleting_from_an_empty_linked_list():
+    l_list = SinglyLinkedList()
+    with raises(ValueError):
+        l_list.delete_node(target_data=0)
+
+
+def test_should_raise_error_when_deleting_using_a_missing_target(linked_list):
+    with raises(ValueError):
+        linked_list.delete_node(target_data=0)
+
+
+def test_should_delete_node_from_list_head(linked_list):
+    linked_list.delete_node(target_data=10)
+    assert linked_list.head.data == 11
+    assert linked_list.head.next.data == 12
+    assert linked_list.head.next.next == None
+
+
+def test_should_delete_node_from_middle_of_list(linked_list):
+    linked_list.delete_node(target_data=11)
+    assert linked_list.head.data == 10
+    assert linked_list.head.next.data == 12
+    assert linked_list.head.next.next == None
+
+
+def test_should_delete_first_duplicate():
+    l_list = SinglyLinkedList()
+    l_list.insert_head(data=10)
+    l_list.insert_head(data=11)
+    l_list.insert_head(data=10)
+    l_list.delete_node(target_data=10)
+    assert l_list.head.data == 11
+    assert l_list.head.next.data == 10
+    assert l_list.head.next.next == None
 
 
 def test_should_print_linked_list(linked_list):
